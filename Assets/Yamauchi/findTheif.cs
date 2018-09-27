8f343af3568a413c0f3678ea14575b37de929d52
Ryoga_Yamauchi <ya.ryouga.ma@docomo.ne.jp>
2018-09-27 19:27:31 +0900

    update script animation

--- Assets/Yamauchi/findTheif.cs
+++ Assets/Yamauchi/findTheif.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class findTheif : MonoBehaviour {
     public Material[] texutureMark = new Material[2];
     GameObject excrumationMark;
+    public Material[] theifTexture = new Material[8];
+    GameObject theif;
+    public AudioClip[] music = new AudioClip[5]; 
+    private AudioSource audioSource;
+    int i = 0;
 	// Use this for initialization
 	void Start () {
         excrumationMark = GameObject.Find("excrumationMark");
+        theif = GameObject.Find("theif");
     }
 	
 	// Update is called once per frame
@@ -22,10 +28,20 @@ public class findTheif : MonoBehaviour {
         //Destroy(this);
         Debug.Log("in a time");
         excrumationMark.GetComponent<Renderer>().material = texutureMark[0];
+        audioSource = this.GetComponent<AudioSource>();
+        audioSource.clip = music[3];
+        audioSource.Play ();
+        MoveRightTheif();
 
     }
     private void OnTriggerExit(Collider other)
     {
         excrumationMark.GetComponent<Renderer>().material = texutureMark[1];
+    }
+    private void MoveRightTheif(){
+        i++;
+        theif.GetComponent<Renderer>().material = theifTexture[i];
+        Invoke("MoveTheif", 0.1f);
+        if (i > 6) i = 0;
     }
 }
