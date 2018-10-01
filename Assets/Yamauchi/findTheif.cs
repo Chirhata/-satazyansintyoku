3f8b8c0196ae2ba524fe5720b373302e962c40f1
Ryoga_Yamauchi <ya.ryouga.ma@docomo.ne.jp>
2018-10-01 12:36:36 +0900

    Sample 1 Complation

--- Assets/Yamauchi/findTheif.cs
+++ Assets/Yamauchi/findTheif.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 public class findTheif : MonoBehaviour {
     public Material[] texutureMark = new Material[2];
     GameObject excrumationMark;
-    public Material[] theifTexture = new Material[8];
+    public Material[] theifTexture = new Material[16];
     GameObject theif;
     public AudioClip[] music = new AudioClip[5]; 
     private AudioSource audioSource;
+    private int state = 1;
+    private float timeleft;
     int i = 0;
+    int j = 0;
 	// Use this for initialization
 	void Start () {
         excrumationMark = GameObject.Find("excrumationMark");
@@ -19,29 +23,107 @@ public class findTheif : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timeleft -= Time.deltaTime;
+        if (timeleft <= 0.0)
+        {
+            timeleft = 0.1f;
+            theif.GetComponent<Renderer>().material = theifTexture[i];
+            i++;
+        }
+        if (i > 15) i = 0;
 	}
 
     private void OnTriggerEnter(Collider other)
     {
         
-        //Destroy(this);
-        Debug.Log("in a time");
-        excrumationMark.GetComponent<Renderer>().material = texutureMark[0];
-        audioSource = this.GetComponent<AudioSource>();
+
+        //Debug.Log("hit object!");
+        excrumationMark.GetComponent<Renderer>().material = texutureMark[0];  //ビックリマークのテクスチャ変更
+        audioSource = this.GetComponent<AudioSource>(); 
         audioSource.clip = music[3];
         audioSource.Play ();
-        MoveRightTheif();
+        Invoke("StateCheck", 1.0f);
+        Invoke("TurnOffTrigger", 1.1f);
 
     }
+
+    //ライトが対象から外れた時の動作
     private void OnTriggerExit(Collider other)
     {
-        excrumationMark.GetComponent<Renderer>().material = texutureMark[1];
+        excrumationMark.GetComponent<Renderer>().material = texutureMark[1];  //ビックリマークのテクスチャ変更
+        CancelInvoke();
     }
-    private void MoveRightTheif(){
-        i++;
-        theif.GetComponent<Renderer>().material = theifTexture[i];
-        Invoke("MoveTheif", 0.1f);
-        if (i > 6) i = 0;
+
+
+        // 泥棒が右に動く動作
+    private void MoveRightTheif()
+    {
+        theif.GetComponent<Animator>().SetBool("RightTrigger", true);
+
     }
+
+        //泥棒が左に動く動作
+    private void MoveLeftTheif()
+    {
+        theif.GetComponent<Animator>().SetBool("LeftTrigger", true);
+
+    }
+
+    public void StateCheck(){
+
+        switch (state)
+        {
+            //一番左にいる時
+            case 1:
+
+                MoveRightTheif();
+                state = 3;
+                Debug.Log("in Center");
+                break;
+
+            //一番右にいる時
+            case 2:
+
+                MoveLeftTheif();
+                state = 3;
+                Debug.Log("in Center");
+                break;
+            //どちらも行ける時
+            case 3:
+                int temp;
+                temp = Random.Range(0, 2);
+
+
+                if (temp == 1)
+                {
+                    state = 2;
+                    MoveRightTheif();
+                    Debug.Log("in Right");
+                    break;
+                }
+
+                else
+                {
+                    state = 1;
+                    MoveLeftTheif();
+                    Debug.Log("in Left");
+                    break;
+                }
+                break;
+        }
+    }
+
+    private void TurnOffTrigger(){
+        theif.GetComponent<Animator>().SetBool("RightTrigger", false);
+        theif.GetComponent<Animator>().SetBool("LeftTrigger", false);
+    }
+
+    //毎フレーム泥棒のテクスチャを変更する
+    private void ChangeMaterial(int i)
+    {
+        theif.GetComponent<Renderer>().material = theifTexture[j];
+        j++;
+        if (j > i+8) j = 0;
+    }
+
 }
